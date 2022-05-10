b1670e4402d59d8ecad0df05fffdab2c28a13320
crayon-big-new <zx1522202417@163.com>
2022-05-10 21:08:08 +0800

    Site updated: 2022-05-10 21:08:07

--- js/music.js
+++ js/music.js
@@ -5,7 +5,7 @@ const ap = new APlayer({
     audio: [{
         name: 'Young For You',
         artist: 'GALA',
-        url: 'https://m704.music.126.net/20211212133437/6298f876daf4b3a20b984659f71f8968/jdyyaac/000c/560b/5153/7f43625368aa52c4fbb0f968fa2007d2.m4a?authSecret=0000017dad0be60506550aa4681408a0',
-        cover: 'https://baike.baidu.com/pic/young%20for%20you/9755378/1/3801213fb80e7bec02f310c12c2eb9389b506be0?fr=lemma&ct=single',
+        url: 'https://music.163.com/outchain/player?type=2&id=1304931036',
+        cover: '/img/default.png',
     }]
 });
