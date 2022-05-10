b699cb4e59b1c448d2e516db49813ff18f939e76
crayon-big-new <zx1522202417@163.com>
2022-05-10 21:32:19 +0800

    Site updated: 2022-05-10 21:32:18

--- js/music.js
+++ js/music.js
@@ -2,10 +2,13 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     mini: true,
     autoplay:true,
+    loop:one,
+   	volume:0.4,
+   	theme: '#eeeeee'
     audio: [{
-        name: 'Young For You',
-        artist: 'GALA',
+        name: 'Last Dance',
+        artist: 'BIGBANG',
         url: '/audio/lastdance.mp3',
-        cover: '/img/default.png',
+        cover: '',
     }]
 });
