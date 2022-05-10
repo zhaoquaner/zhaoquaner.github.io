f64ea8a18835458e214e636675ba753ba3b8e420
crayon-big-new <zx1522202417@163.com>
2022-05-10 20:22:16 +0800

    Site updated: 2022-05-10 20:22:16

---
+++ js/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    mini: true,
+    autoplay:true,
+    audio: [{
+        name: 'Young For You',
+        artist: 'GALA',
+        url: 'https://m10.music.126.net/20220510204414/92162f3a9a640608b5cb51d2fe6afbfa/yyaac/obj/wonDkMOGw6XDiTHCmMOi/14056217759/d067/7974/0a1f/dd4d09570137a382c30ec930ec835ae2.m4a',
+        cover: 'https://baike.baidu.com/pic/young%20for%20you/9755378/1/3801213fb80e7bec02f310c12c2eb9389b506be0?fr=lemma&ct=single',
+    }]
+});
