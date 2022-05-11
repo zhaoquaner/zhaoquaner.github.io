09aa4f69b1cb72317ac1f58e224d50654f133f37
crayon-big-new <zx1522202417@163.com>
2022-05-11 16:31:27 +0800

    Site updated: 2022-05-11 16:31:25

--- js/music.js
+++ js/music.js
@@ -2,17 +2,32 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     mini: true,
     autoplay:false,
-    loop:'one',
-   	volume:0.2,
+    loop:'all',
+   	volume:0.1,
     audio: [{
-        name: 'Last Dance',
+        name: 'Still Life',
         artist: 'BIGBANG',
         url: 'https://crayon-1302863897.cos.ap-beijing.myqcloud.com/audio/stilllife.mp3',
         cover: '',
-    }]
+    },
+    {
+        name: 'Last Dance',
+        artist: 'BIGBANG',
+        url: 'https://crayon-1302863897.cos.ap-beijing.myqcloud.com/audio/lastdance.mp3',
+        cover: '',
+    },
+    ]
 });
 
+
+
 let isPlaying = false;
+
+// 绑定音乐播放完 事件
+ap.on('ended', function () {
+    ap.skipForward();
+    isPlaying = true; 
+});
 
 document.getElementsByClassName("nav-link")[5].onclick = function() {
 	if(!isPlaying) {
